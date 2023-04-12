b7be45c094a7d58083e243483d587ef9b32ae71b
Rakesh_Shinde <rakesh_shinde1@persistent.com>
2023-04-12 21:19:23 +0530

    added assessment file

--- AssesmentTools/force-app/main/default/lwc/relatedListComp/__tests__/relatedListComp.test.js
+++ AssesmentTools/force-app/main/default/lwc/relatedListComp/__tests__/relatedListComp.test.js
@@ -1,5 +1,36 @@
 import { createElement } from 'lwc';
 import RelatedListComp from 'c/relatedListComp';
+import getContacts from '@salesforce/apex/RelatedHandler.getContacts';
+import deleteContacts from '@salesforce/apex/RelatedHandler.deleteContacts';
+
+
+const mockdata = require('./data/result.json');
+const mockdelRes = require('./data/deleterec.json');
+
+async function flushPromises() {
+    return Promise.resolve();
+}
+
+jest.mock(
+    "@salesforce/apex/RelatedHandler.getContacts",
+    () => {
+        const { createApexTestWireAdapter } = require("@salesforce/sfdx-lwc-jest");
+        return {
+            default: createApexTestWireAdapter(jest.fn()),
+        };
+    },
+    { virtual: true }
+);
+
+jest.mock(
+    "@salesforce/apex/RelatedHandler.deleteContacts",
+    () => {
+        return {
+            default: jest.fn()
+        };
+    },
+    { virtual: true }
+);
 
 describe('c-related-list-comp', () => {
     afterEach(() => {
@@ -15,12 +46,69 @@ describe('c-related-list-comp', () => {
             is: RelatedListComp
         });
 
-        // Act
         document.body.appendChild(element);
         const ele = element.shadowRoot.querySelector('lightning-button');
         ele.click();
-        // Assert
-        // const div = element.shadowRoot.querySelector('div');
+        expect(ele).toBeDefined();
+    });
+    it('Testing lightning table with data ', async() => {
+        // Arrange
+        const element = createElement('c-related-list-comp', {
+            is: RelatedListComp
+        });
+        getContacts.emit(mockdata);
+        document.body.appendChild(element);
+        const ele = element.shadowRoot.querySelector('lightning-datatable');
+        ele.dispatchEvent(new CustomEvent('sort',{
+            detail:{fieldName:'FirstName',sortDirection:'DESC'}
+        }));
+        await flushPromises();
+        expect(ele).toBeDefined();
+    });
+
+    it('Testing lightning table with actions ', async() => {
+        // Arrange
+        const element = createElement('c-related-list-comp', {
+            is: RelatedListComp
+        });
+        getContacts.emit(mockdata);
+        document.body.appendChild(element);
+        const ele = element.shadowRoot.querySelector('lightning-datatable');
+        ele.dispatchEvent(new CustomEvent('rowaction',{
+            detail:{action: {name:'edit'},row:{Id:"0035i00000PJAMfAAP"}}
+        }));
+        await flushPromises();
+        const ele1 = element.shadowRoot.querySelector('lightning-datatable');
+        deleteContacts.mockResolvedValue(mockdelRes);
+
+        ele1.dispatchEvent(new CustomEvent('rowaction',{
+            detail:{action: {name:'delete'},row:{Id:"0035i00000PJAMfAAP"}}
+        }));
+       // await flushPromises();
+        await flushPromises();
+        expect(ele).toBeDefined();
+    });
+
+    it('Testing lightning table with actions catch block ', async() => {
+        // Arrange
+        const element = createElement('c-related-list-comp', {
+            is: RelatedListComp
+        });
+        getContacts.emit(mockdata);
+        document.body.appendChild(element);
+        const ele = element.shadowRoot.querySelector('lightning-datatable');
+        ele.dispatchEvent(new CustomEvent('rowaction',{
+            detail:{action: {name:'edit'},row:{Id:"0035i00000PJAMfAAP"}}
+        }));
+        await flushPromises();
+        const ele1 = element.shadowRoot.querySelector('lightning-datatable');
+        deleteContacts.mockRejectedValue({});
+
+        ele1.dispatchEvent(new CustomEvent('rowaction',{
+            detail:{action: {name:'delete'},row:{Id:"0035i00000PJAMfAAP"}}
+        }));
+       // await flushPromises();
+        await flushPromises();
         expect(ele).toBeDefined();
     });
 });
